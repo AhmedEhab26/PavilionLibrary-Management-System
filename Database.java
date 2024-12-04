d2c325ceacbd48a47ea86f3f22ae4df877358133
AhmedEhab26 <56802604+AhmedEhab26@users.noreply.github.com>
2024-12-04 21:05:59 +0200

    Update Database.java

--- Database.java
+++ Database.java
@@ -6,7 +6,7 @@ public class Database {
 
     private String user = "root";
     private String password = "";
-    private String url = "jdbc:mysql://localhost/carrentalsystem";
+    private String url = "jdbc:mysql://localhost/";
     private Statement statement;
 
     public Database() {
